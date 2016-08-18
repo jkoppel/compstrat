26a6a684e51b6c4c4fe0fe60621ad20f85937896
James Koppel <jamesbkoppel@yahoo.com>
2016-08-18 16:33:25 -0400

    It boulds again!

--- examples/examples.hs
+++ examples/examples.hs
@@ -19,7 +19,10 @@ import Control.Monad.State ( MonadState )
 import Control.Monad.Trans.Maybe ( MaybeT )
 
 import Data.Comp.Multi
+import Data.Comp.Multi.Derive
+import Data.Comp.Multi.HFoldable
 import Data.Comp.Multi.HTraversable
+import qualified Data.Comp.Ops as O ( (:&:)(..) )
 
 import Data.Map ( Map )
 import qualified Data.Map as Map
@@ -29,9 +32,12 @@ import Data.Comp.Multi.Strategic
 import Data.Comp.Multi.Strategy.Classification
 import Data.Comp.Multi.Strategy.Derive
 
+import Tarski.Language.Java.Parametric.Full
+import Tarski.SCM.Src
+import Tarski.Sin.Compdata.Derive ( smartFConstructors )
 
-data Label = Label !Int -- other fields omitted
-  deriving ( Eq, Ord, Read, Show )
+-- data Label = Label !Int -- other fields omitted
+--  deriving ( Eq, Ord, Read, Show )
 
 getName' :: (Ident :<: f, ClassDecl :<: f) => GTranslateM Maybe (Term f) String
 getName' (project -> (Just (ClassDecl _ (project -> (Just (Ident s))) _ _ _ _))) = return s
@@ -48,6 +54,11 @@ extractLabels' s = return $ Map.singleton (getAnn s) s
 extractLabels :: (DistAnn f Label f', DynCase (Term f') i, HFoldable f') => Term f' :=> (Map Label (Term f' i))
 extractLabels = runIdentity . (crushtdT $ promoteTF $ addFail extractLabels')
 
+-- | Extract the annotation from the top of an annotated term
+getAnn :: (DistAnn s p s') => Term s' :=> p
+getAnn = annSnd . projectA . unTerm
+  where
+    annSnd (_ O.:&: x) = x
 
 
 --------------------------------------------------------------------------------
@@ -55,18 +66,17 @@ extractLabels = runIdentity . (crushtdT $ promoteTF $ addFail extractLabels')
 
 -- | A sort denoting the top-level sort seen in a source file. To use in a CDT, create a constructor
 --   that injects another sort into TopL. E.g.: CompilationUnitIsTop :: f e CompilationUnitL -> e TopL
-data TopL 
+--data TopL 
+--
+--data SourceFileL
+--data SourceFile e l where
+--  SourceFile :: Bool     -- whether the file is changed. 
+--             -> FilePath -- File path of the corresponding source file
+--             -> e TopL   -- Contents of the source file
+--             -> SourceFile e SourceFileL
 
-data SourceFileL
-data SourceFile e l where
-  SourceFile :: Bool     -- whether the file is changed. 
-             -> FilePath -- File path of the corresponding source file
-             -> e TopL   -- Contents of the source file
-             -> SourceFile e SourceFileL
-
-deriveAll [''SourceFile]
-derive [makeDynCase] [''SourceFile]
-
+--derive [makeHFunctor, makeHTraversable, makeHFoldable, makeEqHF, makeShowHF, makeOrdHF, smartFConstructors, makeDynCase]
+--       [''SourceFile]
 
 trackingFiles' :: (Applicative m, Monad m, SourceFile :<: f, DistAnn f a f', HFunctor f', HFunctor f, RemA f' f) => GRewriteM m (Term f') -> RewriteM m (Term f') SourceFileL
 trackingFiles' f s@(project' -> Just (SourceFile _ x e)) = do e' <- f e
@@ -83,12 +93,6 @@ trackingFiles :: (Applicative m, Monad m, HTraversable f', DistAnn f a f', HFunc
 trackingFiles f = prunetdR $ promoteRF $ tryR (trackingFiles' f)
 
 
--- | Extract the annotation from the top of an annotated term
-getAnn :: (DistAnn s p s') => Term s' :=> p
-getAnn = annSnd . projectA . unTerm
-  where
-    annSnd (_ O.:&: x) = x
-
 --------------------------------------------------------------------------------
 
 
@@ -101,12 +105,16 @@ targetLabel l f t@(Term (projectA -> _ O.:&: l'))
                                  | otherwise = mzero
 
 
+type JavaProg = JavaProjLab [SourceFileL]
+
 runJavaRewrite :: (DynCase JavaProjLab i) => RewriteM (MaybeT (MaybeT Identity)) JavaProjLab i -> JavaProg -> JavaProg
 runJavaRewrite r = runIdentity . (tryR $ trackingFiles $ onebuR $ promoteRF r)
 
 delete' :: (HTraversable f, DistAnn f Label f', Stmt :<: f, HFunctor f) => Rewrite (Term f') StmtL
-delete' _ = return $ annotateNonlabel iEmpty
+delete' _ = return $ ann Nonlabel iEmpty
 
 -- | Replaces the statement with the target label with the empty statement
 delete :: (Monad m) => Label -> JavaProg -> m JavaProg
 delete l = return . (runJavaRewrite $ targetLabel l delete')
+
+main = return ()
